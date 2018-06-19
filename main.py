41b2523ece13bec9bcf39055a51d421b00461b51
jarlvs <premshinde13@gmail.com>
2018-06-19 14:59:11 +0530

    added text check in elements

--- main.py
+++ main.py
@@ -14,21 +14,52 @@ import json
 # repeat()
 
 
+download_dir = "B1mg.csv" 
+csv = open(download_dir, "w") 
+columnTitleRow = "Drugname, Companyname, Composition\n"
+csv.write(columnTitleRow)
+
+
 
 mainurl='https://www.1mg.com/'
 
 
-
+# get all data we want from unique drug
 def getComposition(url):
 	response = requests.get(url)
 	html = response.content
 	soup = BeautifulSoup(html,'lxml')
+
 	for drug in soup.find_all(class_="DrugInfo__drug-name-heading___adCs-"):
-		print 'Drugname '+ (drug.text)
+		Drugname = drug.text
+		if(len(Drugname)>0):
+			print 'Drugname ' + Drugname
+		else:
+			Drugname = " "
+
+
 	for company in soup.find_all(class_="DrugInfo__company-name___39Abk"):
-		print 'Company '+ (company.text)
-	for composition in soup.find_all(class_="saltInfo DrugInfo__salt-name___2-9Vh"):
-		print 'Composition '+ composition.a.text
+		Companyname = company.text
+		if(len(Companyname)>0):
+			print 'Companyname ' + Companyname
+		else:
+			Companyname = " "
+
+
+	for composition in soup.find_all(class_="saltInfo"):
+		Composition = composition.a.text
+		if(len(Composition)>0):
+			print 'Composition '+composition.a.text
+		else:
+			Composition = " ";
+
+
+
+
+	row = Drugname + ", " + Companyname + ", " + Composition + "\n"
+	csv.write(row)
+
+
 
 
 
@@ -41,10 +72,11 @@ def getData(url):
 		getComposition(mainurl+a.get('href'))
 	
 
+# get urls of all urls which will open corresponding number of drugs of particular drug  
 
 def allTabdata():
-	tab=1
-	while (tab<56):
+	tab=2
+	while (tab<59):
 		dataurl="https://www.1mg.com/drugs-listaz?api=true&pageNumber="+str(tab)+"&name=B&_=1529314636770"
 		print dataurl
 		resp = requests.get(url=dataurl)
